baee2fba495d6bef47117da89da265c26a548e13
Evans <ngugie649@gmail.com>
2023-06-24 03:32:03 +0300

    react charts

--- src/componemnts/Sidepanel/Sidebar.jsx
+++ src/componemnts/Sidepanel/Sidebar.jsx
@@ -1,10 +1,30 @@
 import {GrDashboard} from 'react-icons/gr'
 import { Link } from 'react-router-dom';
+import DashboardPic from '../../assets/Dashboard.jpg';
+import avatarPic from '../../assets/avatar.jpg'
 import './Sidebar.css'
+import Dashboard from '../../pages/Dashboard/Dashboard';
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <ul>
+        <div className="card">
+        <img src={avatarPic} alt="piv" />
+        <Link to="/" className='lnk'>Dashboard</Link>
+        </div>
+        <div className="card">
+        <img src={avatarPic} alt="piv" />
+        <Link to="/products" className='lnk'>Products</Link>
+        </div>
+        <div className="card">
+        <img src={avatarPic} alt="piv" />
+        <Link to="/achievements" className='lnk'>Achievements</Link>
+        </div>
+        <div className="card">
+        <img src={avatarPic} alt="piv" />
+        <Link to="/Expenses" className='lnk'>Expenses</Link>
+        </div>
+        
+      {/* <ul>
         <li><GrDashboard className='Gcon'/>
           <Link to="/" className='lnk'>Dashboard</Link>
         </li>
@@ -23,7 +43,7 @@ const Sidebar = () => {
         <li>
             <Link to="/Sales" className='lnk'>Sales</Link>
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,25 +1,97 @@
-function Dashboard() {
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Line, Bar, Pie, Doughnut, Radar } from 'react-chartjs-2';
+
+const Dashboard = () => {
+  const [chartData, setChartData] = useState({});
+  const [commission, setCommission] = useState(0);
+  const [otherData, setOtherData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('your-api-endpoint');
+        const data = response.data;
+
+        // Process the data and extract necessary information for charts and cards
+        const chartData = processChartData(data);
+        const commission = data.commission;
+        const otherData = data.otherData;
+
+        // Update state variables
+        setChartData(chartData);
+        setCommission(commission);
+        setOtherData(otherData);
+      } catch (error) {
+        console.log('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const processChartData = (data) => {
+    // Process the data to create chart datasets
+    // Modify this function based on your data structure and chart requirements
+    // Return an object with the chart data configuration
+    return {
+      labels: ['Label 1', 'Label 2', 'Label 3'],
+      datasets: [
+        {
+          label: 'Chart 1',
+          data: [10, 20, 30],
+          backgroundColor: 'rgba(75, 192, 192, 0.6)',
+        },
+        {
+          label: 'Chart 2',
+          data: [15, 25, 35],
+          backgroundColor: 'rgba(54, 162, 235, 0.6)',
+        },
+        // Add more datasets for other charts
+      ],
+    };
+  };
+
   return (
     <div className="dashContainer">
       <h1>Dashboard</h1>
-      <p>
-        If the content inside your sidebar starts at the top of the window
-        instead of below the sidebar, it might be due to the CSS styling applied
-        to your sidebar or the structure of your layout. Here are a few
-        suggestions to fix this issue: Apply CSS to Sidebar: Make sure that you
-        have applied appropriate CSS styles to your sidebar component to
-        position it correctly and create space for the content. You can use CSS
-        properties like position: fixed or position: sticky to keep the sidebar
-        in place while allowing the content to flow below it. Adjust Layout
-        Structure: Check the structure of your layout component and ensure that
-        the sidebar and content sections are properly organized. It's
-        recommended to use CSS flexbox or grid layout to create a consistent and
-        responsive layout. You can have a container div for the entire layout,
-        with separate divs for the sidebar and content sections. Example
-        structure:
-      </p>
+
+      <div className="chartContainer">
+        <h2>Charts</h2>
+
+        <div className="chart">
+          <Line data={chartData} options={/* chart options */} />
+        </div>
+
+        <div className="chart">
+          <Bar data={chartData} options={/* chart options */} />
+        </div>
+
+        <div className="chart">
+          <Pie data={chartData} options={/* chart options */} />
+        </div>
+
+        <div className="chart">
+          <Doughnut data={chartData} options={/* chart options */} />
+        </div>
+
+        <div className="chart">
+          <Radar data={chartData} options={/* chart options */} />
+        </div>
+      </div>
+
+      <div className="cardContainer">
+        <h2>Cards</h2>
+
+        <div className="card">
+          <h3>Commission</h3>
+          <p>{commission}</p>
+        </div>
+
+        {/* Add more cards for other data */}
+      </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
